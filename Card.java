dd45e9d4108e759d1c363cb09276edad0e438dc5
Sarah Bethea <sarahbethea@sarahs-mbp.lan>
2024-02-17 13:35:26 -0700

    first commit

---
+++ Card.java
@@ -0,0 +1,44 @@
+package week06CodingProjectVersion2;
+
+public class Card {
+
+	private String name;
+	private int value;
+	
+	
+
+	
+	public Card(String name, int value) {
+		this.name = name;
+		this.value = value;
+		
+	}
+	
+	
+	
+	public void describe() {
+		System.out.println(this.name + ". Value: " + this.value);
+	}
+
+	
+	public String getName() {
+		return name;	
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public int getValue() {
+		return value;
+	}
+	
+	public void setValue(int value) {
+		this.value = value;
+	}
+
+
+
+	
+	
+}
